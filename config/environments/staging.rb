8c07034ec8f0e43662a34aaf2fbe6aab31afbe96
Kyle Foo <fung933@gmail.com>
2017-02-22 03:47:19 +0800

    updated staging file

---
+++ config/environments/staging.rb
@@ -0,0 +1,29 @@
+Rails.application.configure do
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+  config.serve_static_files = false
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = false
+  config.assets.digest = true
+  config.log_level = :info
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+  config.log_formatter = ::Logger::Formatter.new
+  config.active_record.dump_schema_after_migration = false
+  config.assets.debug = true
+  config.assets.raise_runtime_errors = true
+  config.action_mailer.default_url_options = { host: 'www.boltbutton.com' } 
+  config.action_controller.asset_host = "http://www.boltbutton.com"
+  config.force_ssl = false
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
+end
