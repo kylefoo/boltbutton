9cf91999810a0e9870a5de88517701fa03ee20b5
Kyle Foo <fung933@gmail.com>
2017-02-22 12:28:15 +0800

    updated capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :normalize_asset_timestamps, %{public/images public/javascripts public/style
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :debug
 
 # Default value for :pty is false
 # set :pty, true
@@ -58,6 +58,14 @@ namespace :deploy do
     end
   end
 end
+
+namespace :bundle do
+  desc "run bundle install and ensure all gem requirements are met"
+  task :install do
+    run "cd #{current_path} && bundle install  --without=test --no-update-sources"
+  end
+end
+before "deploy:restart", "bundle:install"
 
 namespace :rails do
   desc "Tail rails error logs from server"
