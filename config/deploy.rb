3bb839b1f27b39b7216f0405a6390b64250548c7
Kyle Foo <fung933@gmail.com>
2017-02-23 00:37:36 +0800

    syntax update

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,7 +62,7 @@ end
 namespace :bundle do
   desc "run bundle install and ensure all gem requirements are met"
   task :install do
-    run "cd #{current_path} && bundle install  --without=test --no-update-sources"
+    execute "cd #{current_path} && bundle install  --without=test --no-update-sources"
   end
 end
 before "deploy:restart", "bundle:install"
